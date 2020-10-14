d66d28b8692130988a3f4a257e4340c5a1c1f3b5
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-14 01:42:49 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,14 @@
 class project
   
-  attr_reader :
+   attr_reader :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end 
+end
